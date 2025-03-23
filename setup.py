113eb2db06bb145b08ae0018e03ea1705d71c42a
Philip Sadighian <gbr5yn@virginia.edu>
2025-03-23 16:41:40 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="booklover",
+    version="0.1",
+    description='package for storing books and their ratings'
+    url='https://github.com/Ratocracy/m09-booklover'
+    author = 'Philip Sadighian'
+    author_email = 'gbr5yn@virginia.edu'
+    license = 'MIT'
+    packages = ['booklover']
+)
